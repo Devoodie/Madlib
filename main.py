aeb1342c0ebd46c0aab1a09a05e08e7ca4b07739
Devoodie <131494134+Devoodie@users.noreply.github.com>
2023-04-22 01:59:29 -0400

    This is an update!

--- main.py
+++ main.py
@@ -1,33 +1,26 @@
 import string
 
-def makenewstory(name,filelocation):
-   name= Stories(filelocation)
+
 class Stories:
-   def __init__(self, location):
-      self.keywords = []
-      with open(location,"r") as f:
-         self.story = f.read()
+  def __init__(self, location):
+    self.keywords = []
+    with open(location,"r") as f:
+        self.story = f.read()
 
-   def finder(self):
-      for character in enumerate(self.story):
-         if character == "_" and self.story[self.story.find(character)+1] in string.punctuation:
-            return self.story.find(character+"")
-            break
-         elif character == "_" and self.story[self.story.find(character)+1] == " ":
-            return self.story.find(character+" ")
-            break
-
-   def slicer(self):
-      arrayindex = 0
-      placeholderString = ""
-      i=0
-      while self.story.find("_") != -1 and arrayindex<len(self.keywords):
-         resetPoint=self.story.find("_")
-         end_=self.story.find("_")
-         print(resetPoint, "reset")
-         placeholderString = self.story[:resetPoint] + self.keywords[arrayindex] + self.story[self.finder():]
-         self.story = placeholderString
-         arrayindex += 1
-
-Penis = Stories("D:\D\madlib.txt")
-Penis.finder()
+  def finder(self):
+    for count, character in enumerate(self.story):
+      if self.story[count+1] != "_":
+        if character == "_" and self.story[count+1] in string.punctuation:
+          return count+1
+        if character == "_" and self.story[count+1] == " ":
+          print(count)
+          return count+1
+  def slicer(self):
+    arrayindex = 0
+    placeholderString = ""
+    while self.story.find("_") != -1 and arrayindex<len(self.keywords):
+      resetPoint=self.story.find("_")
+      print(resetPoint, "reset")
+      placeholderString = self.story[:resetPoint] + self.keywords[arrayindex] + self.story[self.finder():]
+      self.story = placeholderString
+      arrayindex += 1
