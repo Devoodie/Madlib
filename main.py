aba3f8be4b1c99a6e9137da89e1c38c606887171
Devoodie <131494134+Devoodie@users.noreply.github.com>
2023-04-22 11:56:41 -0400

    Pep 8 Compliant

--- main.py
+++ main.py
@@ -3,23 +3,24 @@ import string
 
 class Stories:
     def __init__(self, location):
-      self.keywords = []
-      with open(location, "r") as f:
-        self.story = f.read()
+        self.keywords = []
+        with open(location, "r") as f:
+            self.story = f.read()
+
     def finder(self):
         for count, character in enumerate(self.story):
-          if self.story[count + 1] != "_":
-            if character == "_" and self.story[count + 1] in string.punctuation:
-              return count + 1
-          if character == "_" and self.story[count + 1] == " ":
-            print(count)
-            return count + 1
+            if self.story[count + 1] != "_":
+                if character == "_" and self.story[count + 1] in string.punctuation:
+                    return count + 1
+            if character == "_" and self.story[count + 1] == " ":
+                print(count)
+                return count + 1
+
     def slicer(self):
         arrayindex = 0
-        placeholderString = ""
         while self.story.find("_") != -1 and arrayindex < len(self.keywords):
-          resetPoint = self.story.find("_")
-          print(resetPoint, "reset")
-          placeholderString = self.story[:resetPoint] + self.keywords[arrayindex] + self.story[self.finder():]
-          self.story = placeholderString
-          arrayindex += 1
+            resetpoint = self.story.find("_")
+            print(resetpoint, "reset")
+            placeholderstring = self.story[:resetpoint] + self.keywords[arrayindex] + self.story[self.finder():]
+            self.story = placeholderstring
+            arrayindex += 1
