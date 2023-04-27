9924ca5446aff34925bcf0cfaaaaa6b6000f672d
Devoodie <131494134+Devoodie@users.noreply.github.com>
2023-04-26 23:57:26 -0400

    Many Funcions Added runstory function added loadstory function added savestory function edited namecheck function added makenewstory function changed to runprogram and also edited There is a problem with the loadstory 'File does not exist' FIXIT!

--- main.py
+++ main.py
@@ -4,20 +4,58 @@ import json
 
 
 class Story:
-    def __init__(self, name, location):
+    def __init__(self, name):
         self.name = name
         self.keywords = {}
+        self.story = ""
+        self.data = {}
+
+    def savestory(self, location):
+        os.mkdir(f"{self.name}")
         with open(location, "r") as f:
             self.story = f.read()
+        self.data = {'keywords': self.keywords, 'story': self.story}
+        with open(self.name+"\\"+self.name+".txt", "w") as savefile:
+            json.dump(self.data, savefile)
 
-    def savestory(self):
-        ogpath = "C:\\Users\\devan\\PycharmProjects\\pythonProject\\"
-        os.mkdir(f"{self.name}")
-        newpath = os.path.join(ogpath, f"{self.name}")
-        with open(newpath + "\\story.txt", "w") as savefile:
-            json.dump(self.story, savefile)
-        with open(newpath + "\\keywords.txt", "w") as savefile:
-            json.dump(self.keywords, savefile)
+    def namecheck(self, stopthatshit=0):
+        recursion = stopthatshit
+        if recursion >= 100:
+            print("FIND A VALID NAME!")
+            exit()
+        elif os.path.exists(self.name + ".txt") is False:
+            pass
+        else:
+            newordelete = str(input("That story already exists! Do you want to create a new one, delete the old one, or load the old one?"))
+            while True:
+                if newordelete == "delete":
+                    os.remove(self.name + ".txt")
+                    break
+                elif newordelete == "new":
+                    self.name = str(input("enter a valid name!"))
+                    self.namecheck(recursion+1)
+                    break
+                elif newordelete == "load":
+                    self.loadstory()
+                    self.runstory()
+                    return 0
+                else:
+                    print("There was a typo! Type delete, new, or load")
+
+    def loadstory(self):
+        with open(self.name+".txt", "r") as savefile:
+            tempdict = json.load(savefile)
+        self.keywords = tempdict['keywords']
+        self.story = tempdict['story']
+
+    def runstory(self):
+        for count, key in enumerate(self.keywords):
+            if count in key:
+                printkey = key.replace(f"{count}", "")
+                self.keywords[key] = input(f'Enter another {printkey}')
+            else:
+                self.keywords[key] = input(f'Enter a {key}')
+        print(self.slicer())
 
     def finder(self):
         for count, character in enumerate(self.story):
@@ -36,6 +74,7 @@ class Story:
             placeholderstring = self.story[:resetpoint] + keywordvalues[arrayindex] + self.story[self.finder():]
             self.story = placeholderstring
             arrayindex += 1
+        return self.story
 
     def dictmaker(self, amount):
         for i in range(amount):
@@ -46,22 +85,32 @@ class Story:
                 self.keywords[wordplaceholder] = ""
 
 
-def makenewstory():
+def runprogram():
     choice = str(input("What do you want to do? Import Story? Random Story? Specific Story?"))
     while True:
         if choice == "import":
-            newstory = Story(input("Enter the name of the story!"), input("Enter the file path!"))
+            newstory = Story(input("Enter the name of the story!"))
+            if newstory.namecheck() == 0:
+                break
             iterations = int(input("How many Keywords are there?"))
             newstory.dictmaker(iterations)
             print(newstory.keywords.keys())
-            newstory.savestory()
+            newstory.savestory(input("Enter the file path of the story to import!"))
+            loadornah = input("do you want to run the story? Yes or no?")
+            if loadornah == "yes":
+                newstory.runstory()
+            if loadornah == "no":
+                break
             break
         elif choice == "random":
             break
         elif choice == "specific":
+            newstory = Story(input("What is the name of the story?"))
+            newstory.loadstory()
+            newstory.runstory()
             break
         else:
             choice = str(input("There was a typo. Enter import, random, or specific."))
 
 
-makenewstory()
+runprogram()
